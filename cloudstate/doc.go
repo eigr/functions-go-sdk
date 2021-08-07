016c34eea5871e0014a635eacf54e9e51b95bba5
Marcel Lanz <marcellanz@n-1.ch>
2021-08-07 20:10:55 +0200

    proto file headers updated. *.pb.go file regenerated.

--- cloudstate/doc.go
+++ cloudstate/doc.go
@@ -3,5 +3,5 @@
 // Use of this source code is governed by the Apache License
 // that can be found in the LICENSE file.
 
-// Package permastate implements the Cloudstate protocol.
+// Package cloudstate implements the Cloudstate protocol.
 package cloudstate
